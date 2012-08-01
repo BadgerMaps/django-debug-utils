c7ec6e5e984803d38e188bcb90d2ebbd0b7a232c
Michał Jaworski <swistakm@gmail.com>
2012-08-01 12:39:52 +0200

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+PACKAGES = find_packages()
+INSTALL_REQUIRES = [
+    'Django',
+]
+setup(
+    name='django-debug-utils',
+    version='0.0.1',
+    description='Little tools useful for debugging',
+    author = "Eric Holscher",
+    author_email = "eric@ericholscher.com",
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    zip_safe=False,
+)
